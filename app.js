e650042e0a63137e21c573ac3a6f86e5a4327766
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2018-04-18 01:41:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
   }		
 });
 bot.on("message", async message => {
-  //try triggering on a different event and using client.voiceConnections property
+  //try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
   if(message.author.bot) return;
   if(message.content.indexOf("z join fleet voice please") !== 0) return;
    
