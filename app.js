40c47069968bef1a82d343a97bd6edee8497f670
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2018-04-18 00:07:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {    
   				// join the correct voice channel 
-//  let vChannel = bot.channels.get(process.env.VCHANNEL);  
-//   vChannel.join()
-//   		.then(connection => console.log('Connected'))
-//  		.catch(console.error);  		
+  let vChannel = bot.channels.get(process.env.VCHANNEL);  
+   vChannel.join()
+   		.then(connection => console.log('Connected'))
+  		.catch(console.error);  		
   				//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
 });
 
