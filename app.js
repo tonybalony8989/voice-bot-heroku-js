2706e96a0b15658c053ea28e40febee540496b93
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2018-04-18 23:57:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,9 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
 bot.on("message", async message => {
   			//try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
   if(message.author.bot) return;
-  if(message.channel.guild.id !== process.env.GUILD) return;
+  //if(message.channel.guild.id !== process.env.GUILD) return;
+	//bot is private, only in one guild, thus we only need to check against DM/groupDM which provide an id property
+  if(message.channel.id !== undefined) return;
    
   if(message.content === "z join fleet voice please") {
   	// join the correct voice channel 
