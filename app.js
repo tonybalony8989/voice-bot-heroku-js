3fe2c3fcfbfaf9a69556fea7b991ada949e05466
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2018-04-19 00:19:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,9 +67,10 @@ bot.on("message", async message => {
   			//try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
   if(message.author.bot) return;
 	//only accept commands from within a guild (not DM or groupDM)
-  let guildinfo=message.guild;
+  let guildinfo=message.guild.id
+  	.catch(console.error);	
   if(guildinfo===undefined) return;
-	console.log(message.guild.id);
+	console.log(guildinfo);
   		
   if(message.content === "z join fleet voice please") {
   	// join the correct voice channel 
