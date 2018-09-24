a1e8516045a6b95d54aaa1c06778791ae409f537
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2018-09-23 21:07:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var min=String("0"+d.getUTCMinutes()).slice(-2);
 var sec=String("0"+d.getUTCSeconds()).slice(-2);
 //[11:22:33]
 	
-		hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+member.displayName+' 				`'+member.user.id+'` ***SPEAKING***');	}  
+		hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  **'+member.displayName+'** 				`'+member.user.id+'` ***SPEAKING***');	}  
 });
 
 bot.on("voiceStateUpdate", (oldMember, newMember) =>{
