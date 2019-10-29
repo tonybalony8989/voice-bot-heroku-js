3c55d739b0ef9aff7e4e3d60f0ec20467a17aa43
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-10-28 20:01:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,9 @@ bot.on("ready", () => {
   				// join the correct voice channel 
   let vChannel = bot.channels.get(process.env.VCHANNEL);  
    vChannel.join()
-     	.catch(console.error)  
-   		.then(async function(connection) {
-		        connection.on('speaking', (user, speaking) => {
-				console.log("Speaking")
-				if (!speaking){
-                return
-				}
-				})		
-		})		
-	//.then(connection => console.log('Connected'))
-  	//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
+   		.then(connection => console.log('Connected'))
+  		.catch(console.error);  		
+  				//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
 	//bot.voiceConnections.map(voiceConnection => console.log(voiceConnection));
 });
 
