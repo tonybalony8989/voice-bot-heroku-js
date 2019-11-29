81fa90d6ef118331f98a1142e079d06f7dd9fe54
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-28 22:02:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.on("ready", () => {    
+bot.on("ready", async (oldMember, newMember) => {   
   				// join the correct voice channel 
   let vChannel = bot.channels.get(process.env.VCHANNEL);  
-   vChannel.join()
+  const connection = await vChannel.join()
      	.catch(console.error)  
-	.then(connection => console.log('Connected'))	
+	.then(const connection => console.log('Connected'))	
 	
-
-//	.then(connection => console.log('Connected'))	
-  	//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
-	//bot.voiceConnections.map(voiceConnection => console.log(voiceConnection));
-});
-
-bot.on('presenceUpdate', async (oldMember, newMember) => {
-  console.log('Presence:', newMember.presence)
-
-  if (!newMember.presence || !newMember.presence.game || !newMember.voiceChannel) {
-    return
-  }
-
-  const connection = await newMember.voiceChannel.join()
-
   connection.on('speaking', (user, speaking) => {
     if (speaking) {
       console.log(`I'm listening to ${user.username}`)
     } else {
       console.log(`I stopped listening to ${user.username}`)
     }
-  })
-})
+  })	
+
+
+  	//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
+	//bot.voiceConnections.map(voiceConnection => console.log(voiceConnection));
+});
+
 
 bot.login(process.env.TOKEN);
