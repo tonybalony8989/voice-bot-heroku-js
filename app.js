69a1bba4ac36d7d1bd74b43fdb770dfde4dd67b6
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-29 19:51:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,57 +35,7 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
 		hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+member.displayName+' 				`'+member.user.id+'` ');	}  
 });
 
-bot.on("voiceStateUpdate", (oldMember, newMember) =>{
-  let newUserChannel = newMember.voiceChannel
-  let oldUserChannel = oldMember.voiceChannel
-  let vlChannel = bot.channels.get(process.env.VLCHANNEL);
-  let ttsChannel = bot.channels.get(process.env.TTSCHANNEL);
-  let trackChannel = bot.channels.get(process.env.TRACKCHANNEL);
-  let hChannel = bot.channels.get(process.env.TCHANNEL);
-//	 console.log(newMember.roles.last().name);
-	
-	
-var d = new Date();
-var year=String(d.getUTCFullYear());
-var month=String("0"+(d.getUTCMonth()+1)).slice(-2);
-var day=String("0"+d.getUTCDate()).slice(-2);
-var hour=String("0"+d.getUTCHours()).slice(-2);
-var min=String("0"+d.getUTCMinutes()).slice(-2);
-var sec=String("0"+d.getUTCSeconds()).slice(-2);
-  if (oldUserChannel === undefined) {
-  	//user joined channel
-  	//vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'`'+newMember.user.username+'#'+newMember.user.discriminator+'` ***JOINED*** _'+newUserChannel.name+'_');
-	vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'`'+newMember.user.id+'` ***JOINED*** _'+newUserChannel.name+'_');
-	hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'`'+newMember.user.id+'` ***JOINED*** _'+newUserChannel.name+'_');
-	  //previous line added 2018-09-23
-  	ttsChannel.send(newMember.displayName+' JOINED '+newUserChannel.name.replace(/\s/g, '')+'', { tts: true});
-  	if ((newMember.roles.last().name) === "@everyone") {
-		trackChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'`'+newMember.user.id+'` ***JOINED*** _'+newUserChannel.name+'_');
-  	}
 
-	
-  }
-  else {
-		if (newUserChannel === undefined) {
-			//user left channel
-			vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.id+'` ***LEFT*** _'+oldUserChannel.name+'_');
-			hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.id+'` ***LEFT*** _'+oldUserChannel.name+'_');
-			//previous line added 2018-09-23
-			ttsChannel.send(oldMember.displayName+' LEFT '+oldUserChannel.name.replace(/\s/g, '')+'', { tts: true});
-			}
-		else {
-			//user switched channel
-			if (newUserChannel!=oldUserChannel) {
-				vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.id+'` ***SWITCHED*** _'+oldUserChannel.name+'_ to _'+newUserChannel.name+'_');
-				hChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.id+'` ***SWITCHED*** _'+oldUserChannel.name+'_ to _'+newUserChannel.name+'_');
-				//previous line added 2018-09-23
-				ttsChannel.send(oldMember.displayName+' SWITCHED '+oldUserChannel.name.replace(/\s/g, '')+' to '+newUserChannel.name.replace(/\s/g, '')+'', { tts: true});
-				//str = str.replace(/\s/g, '');
-				//something something
-				}
-			}
-  }		
-});
 bot.on("message", async message => {
   			//try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
   if(message.author.bot) return;
