c66eafe59c4e93aca959d2eefb7bae9e6a5cbc21
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-29 22:04:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,55 @@ bot.on("ready", () => {      				// join the correct voice channel
 });
 
 bot.on("guildMemberSpeaking", (member, speaking) => { 
-	console.log(`${member.displayName} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
-//	if (speaking) {
-//	let hChannel = bot.channels.get(process.env.TCHANNEL);	
-//	hChannel.send(Botdatestring()+member.displayName+' 				`'+member.user.id+'` ');	
-//	}  
+	//console.log(`${member.displayName} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
+	if (speaking.has(1)) {
+	let hChannel = bot.channels.get(process.env.TCHANNEL);	
+	hChannel.send(Botdatestring()+member.displayName+' 				`'+member.user.id+'` ');	
+	}  
 });
 
 bot.on("voiceStateUpdate", (oldState, newState) =>{
   
-  console.log(`${Botdatestring()} ${newState.member.displayName} ${newState.channel.name} ${newState.member.id}`);
+  console.log(`${BotDate()} ${newState.member.displayName} ${newState.channel.name} ${newState.member.id}`);
 });
 
+bot.on("voiceStateUpdate", (oldState, newState) =>{
+  let newUserChannel = newState.channel.name;
+  let oldUserChannel = oldState.channel.name;
+  let newName = newState.member.displayName;
+  let newID = newState.member.id;
+  let vlChannel = bot.channels.get(process.env.VLCHANNEL);
+  let ttsChannel = bot.channels.get(process.env.TTSCHANNEL);
+  let trackChannel = bot.channels.get(process.env.TRACKCHANNEL);
+  let hChannel = bot.channels.get(process.env.TCHANNEL);
+	
+  if (oldUserChannel === undefined) {  	//user joined channel  	
+	vlChannel.send(BotDate()+newName+'`'+newID+'` ***JOINED*** _'+newUserChannel+'_');
+	hChannel.send(BotDate()+newName+'`'+newID+'` ***JOINED*** _'+newUserChannel+'_');	  
+  	ttsChannel.send(newMember.displayName+' JOINED '+newUserChannel.name.replace(/\s/g, '')+'', { tts: true});
+		// 	if ((newState.member.roles.last().name) === "@everyone") {
+		//		trackChannel.send(BotDate()+newName+'`'+newID+'` ***JOINED*** _'+newUserChannel+'_');
+		// 	}
+		// check out .highest  https://discord.js.org/#/docs/main/master/class/GuildMemberRoleStore
+  }
+  else {
+	    let oldName = oldState.member.displayName;
+		let oldID = oldState.member.id;  
+		if (newUserChannel === undefined) {	//user left channel
+			vlChannel.send(BotDate()+oldMember.displayName+'`'+oldMember.user.id+'` ***LEFT*** _'+oldUserChannel+'_');
+			hChannel.send(BotDate()+oldMember.displayName+'`'+oldMember.user.id+'` ***LEFT*** _'+oldUserChannel+'_');
+			ttsChannel.send(oldMember.displayName+' LEFT '+oldUserChannel.name.replace(/\s/g, '')+'', { tts: true});
+			}
+		else {	//user switched channel
+			if (newUserChannel!=oldUserChannel) {
+				vlChannel.send(BotDate()+oldName+'`'+oldID+'` ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
+				hChannel.send(BotDate()+oldName+'`'+oldID+'` ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
+				ttsChannel.send(oldName+' SWITCHED '+oldUserChannel.replace(/\s/g, '')+' to '+newUserChannel.replace(/\s/g, '')+'', { tts: true});
+				//str = str.replace(/\s/g, '');
+				}
+			}
+  }		
+});
 bot.on("message", async message => {
   			//try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
   if(message.author.bot) return;
@@ -53,13 +90,14 @@ bot.on('disconnect', function(msg, code) {
 
 bot.login(process.env.TOKEN);
 
-function Botdatestring() {
-var d = new Date();
-var year=String(d.getUTCFullYear());
-var month=String("0"+(d.getUTCMonth()+1)).slice(-2);
-var day=String("0"+d.getUTCDate()).slice(-2);
-var hour=String("0"+d.getUTCHours()).slice(-2);
-var min=String("0"+d.getUTCMinutes()).slice(-2);
-var sec=String("0"+d.getUTCSeconds()).slice(-2); //[11:22:33]
+
+function BotDate() {
+	var d = new Date();
+	var year=String(d.getUTCFullYear());
+	var month=String("0"+(d.getUTCMonth()+1)).slice(-2);
+	var day=String("0"+d.getUTCDate()).slice(-2);
+	var hour=String("0"+d.getUTCHours()).slice(-2);
+	var min=String("0"+d.getUTCMinutes()).slice(-2);
+	var sec=String("0"+d.getUTCSeconds()).slice(-2); //[11:22:33]
 return('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  ')
 }
