68bf5474fcac55a6b28c688385296c312925214a
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-30 00:17:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ bot.on("ready", () => {      				// join the correct voice channel
    vChannel.join()
      	.catch(console.error)  
 	.then(connection => {console.log('Connected');
-	console.log(`${bot.voice.connections.status}`);})	
+	console.log(`${bot.voice.connections.last().status}`);})	
   	//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
 	//bot.voiceConnections.map(voiceConnection => console.log(voiceConnection));
 });
