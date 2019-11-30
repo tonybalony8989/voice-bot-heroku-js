58576b89236575b0dd69d09758f9c8e562919a86
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-30 14:16:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ bot.on("ready", () => {      				// join the correct voice channel
      	.catch(console.error)  	
 	 .then(async function(connection) {
 		         connection.on('speaking', (user, speaking) => {
-					console.log(`${user.username} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
-					 if (speaking.has(1)) {
-						 let hChannel = bot.channels.get(process.env.TCHANNEL);	
-						 hChannel.send(BotDate()+user.username+' 				`'+user.id+'` ');	
-						//username doesn't give the displayName within the guild
+				 console.log("speaking async");
+					//console.log(`${user.username} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
+					// if (speaking.has(1)) {
+						// let hChannel = bot.channels.get(process.env.TCHANNEL);	
+						// hChannel.send(BotDate()+user.username+' 				`'+user.id+'` ');	
+						////username doesn't give the displayName within the guild
 					 } 
 				 })		
 		 })		
