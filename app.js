88e60dabbec397cbaa927ee0f86b1e38872d0f6e
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2019-11-30 00:49:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ bot.on("ready", () => {      				// join the correct voice channel
      	.catch(console.error)  
 	 		.then(async function(connection) {
 		        connection.on('speaking', (user, speaking) => {
-					console.log(`${member.displayName} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
+					console.log(`${user.username} ${speaking.has(1)} ${speaking.has(2)} ${speaking.has(3)} ${speaking.has(4)} ${speaking.has(5)} ${speaking.has(6)} ${speaking.has(7)}`);
 					if (speaking.has(1)) {
 						let hChannel = bot.channels.get(process.env.TCHANNEL);	
 						hChannel.send(BotDate()+member.displayName+' 				`'+member.user.id+'` ');	
@@ -73,23 +73,18 @@ bot.on("voiceStateUpdate", (oldState, newState) =>{
   }		
 });
 bot.on("message", async message => {
-  			//try triggering on a different event and using client.voiceConnections property https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=voiceConnections
-  if(message.author.bot) return;
-	//only accept commands from within a guild (not DM or groupDM) message.member is null for the former two  
-  if(message.member===null) return;
-	//console.log(message.member);
-  		
+  if(message.author.bot) return;	//only accept commands from within a guild (not DM or groupDM) message.member is null for the former two  
+  if(message.member===null) return;	//console.log(message.member);  		
   if((message.content === "z join fleet voice please") && ((message.member.roles.highest.name) != "@everyone") ) {
-  	// join the correct voice channel 
-	  
-  let vChannel = bot.channels.get(process.env.VCHANNEL);  
-   vChannel.leave(); 
-   console.log('Left channel - Wait 5 seconds');
+  	// join the correct voice channel 	  
+	let vChannel = bot.channels.get(process.env.VCHANNEL);  
+	vChannel.leave(); 
+	console.log('Left channel - Wait 5 seconds');
 	  setTimeout(function() {
-	vChannel.join()
-   		.then(connection => console.log('Connected - manual. '+message.author.username+' User ID= '+message.author.id))
-  		.catch(console.error);   
-	  }, 5000);
+			vChannel.join()
+			.then(connection => console.log('Connected - manual. '+message.author.username+' User ID= '+message.author.id))
+			.catch(console.error);   
+			}, 5000);
    
   	 }
 });
