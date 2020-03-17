99b7cf50142dd682583733df1b00f770fd1e6b23
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2020-03-17 19:10:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var lastRepair = new Date('2010/01/05 10:11:12');
 var lastChat = new Date('1999/01/05 10:11:12');
 
 bot.on('ready', async () => {      				// join the correct voice channel 
-  let vChannel = bot.channels.get(process.env.VCHANNEL);  
+  track(BotDate()+"ready start");
+	
+   let vChannel = bot.channels.get(process.env.VCHANNEL);  
    await vChannel.join()			 	
 			.then(connection => { BotConn(connection, ":boom: new voice connection", true)
 			    }) 
