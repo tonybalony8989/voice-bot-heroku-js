aed05d0a20e51cfbd0e801e7f326327900c7d3e3
tonybalony8989 <38482479+tonybalony8989@users.noreply.github.com>
2020-04-12 22:11:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -404,7 +404,7 @@ function track(testMsg) {
 }
 function tChanSend(tChan, tMsg, TTSstate) { //try to catch some errors that intermitently show up as 'HTTPError'
 	if (TTSstate==true) {
-		tMsg+=", { tts: true}";
+		tMsg+=, { tts: true};
 		tChan.send(tMsg).catch((e) => track(BotDate()+'a send error occurred')).catch(console.log);	
 	}
 	else
